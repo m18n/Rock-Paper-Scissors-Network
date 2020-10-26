a9463220c45d13875ff17f24339bb35580a42dc5
Misha <20xim04@gmail.com>
2020-10-26 22:52:23 +0200

    Edit Names

--- Rock-Paper-Scissors-Network/Rock-Paper-Scissors-Network/main.cpp
+++ Rock-Paper-Scissors-Network/Rock-Paper-Scissors-Network/main.cpp
@@ -1,30 +1,36 @@
 #include<iostream>
 using namespace std;
-void Vxid(int pass, char Log[50])
+struct Player
+{
+	char login[20];
+	int password = 0;
+	int score = 0;
+};
+void SingIn(char login[20],int& password)
 {
 	cout << "Login:";
-	cin >> Log;
+	cin >> login;
 	cout << "Password:";
-	cin >> pass;
+	cin >> password;
 }
-void Singup(char lo[50], int passwor)
+void SingUp(char login[50], int& password)
 {
 	int password1;
 	cout << "Login:";
-	cin >> lo;
+	cin >> login;
 	int i = 0;
 	while (i == 0)
 	{
 		cout << "password:";
-		cin >> passwor;
+		cin >> password;
 		cout << "password:";
 		cin >> password1;
-		if (passwor == password1)
+		if (password == password1)
 		{
-			passwor = passwor;
+			password = password;
 			i = 1;
 		}
-		else if (passwor != password1)
+		else if (password != password1)
 		{
 			cout << "please(pasword=pasword)\n";
 
@@ -33,27 +39,17 @@ void Singup(char lo[50], int passwor)
 
 
 }
-struct Player
-{
-	char Login[50];
-	int password;
-	int score;
-};
+
 int main() {
-	int true1 = 0;
-	int passw = 0;
-	char Logg[50];
+	Player pl;
+	int choice = 0;
 	cout << "Sing in-1\n";
-	cout << "sing up-2\n";
-	cin >> true1;
-	if (true1 == 1)
-	{
-		Vxid(passw, Logg);
-	}
-	else if (true1 == 2)
-	{
-		Singup(Logg, passw);
-	}
+	cout << "Sing up-2\n";
+	cin >> choice;
+	if (choice == 1)
+		SingIn(pl.login, pl.password);
+	else if (choice == 2)
+		SingUp(pl.login,pl.password);
 	system("pause");
 	return 0;
 }
