d1e20cc371314c8ca0410ea98e3f384b84c76526
Ice159 <60782182+Ice159@users.noreply.github.com>
2020-10-26 13:28:25 +0200

    ВхідОснова

---
+++ Rock-Paper-Scissors-Network/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void Vxid(int pass,char Log[50])
+{
+	cin >> Log; 
+	cin >> pass;
+}
+struct Player
+{
+	char Login[50];
+	int password;
+	int score;
+};
+int main() {
+	int true1;
+	cout <<"Війти-1";
+	cout << "Зареєструватися-2";
+	cin >> true1;
+	if (true1==1)
+	{
+
+	}
+}
