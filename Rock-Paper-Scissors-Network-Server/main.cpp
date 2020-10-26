3c65d0e75452619e98026a170d56b28457f11b2f
Misha <20xim04@gmail.com>
2020-10-26 13:45:26 +0200

    Connection Server

--- Rock-Paper-Scissors-Network-Server/main.cpp
+++ Rock-Paper-Scissors-Network-Server/main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<list>
 #include<WinSock2.h>
 #pragma comment(lib,"ws2_32.lib")
 using namespace std;
+list<SOCKET> conn;
 struct Player {
 	char login[20];
 	int password=0;
@@ -11,26 +13,40 @@ struct Connect {
 	WSAData wsaData;
 	WORD DLLversion;
 	SOCKADDR_IN addr;
+	int sizeofaddr;
 };
-
 void Inithilization(Connect& cn,string ip,short int port) {
 	cn.DLLversion = MAKEWORD(2, 1);
 	if (WSAStartup(cn.DLLversion, &cn.wsaData)!=0) {
 		cout << "Error 1\n";
 		exit(1);
 	}
+	cn.sizeofaddr=sizeof(cn.addr);
 	cn.addr.sin_addr.s_addr = inet_addr(ip.c_str());
 	cn.addr.sin_port = htons(port);
 	cn.addr.sin_family = AF_INET;
+}
+void ConnectSocket(Connect&cn) {
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&cn.addr, sizeof(cn.addr));
+	listen(sListen, SOMAXCONN);
+	SOCKET newConnection;
+	while (true)
+	{
+		newConnection = accept(sListen, (SOCKADDR*)&cn.addr,&cn.sizeofaddr);
+		if (newConnection == 0)
+			cout << "Error Connection\n";
+		else
+			cout << "Connect\n";
+		conn.push_back(newConnection);
+
+	}
 }
 int main() {
 	cout << "start server\n";
 	Connect cn;
 	Inithilization(cn,"192.168.0.103",9999);
-	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
-	bind(sListen, (SOCKADDR*)&cn.addr,sizeof(cn.addr));
-	listen(sListen, SOMAXCONN);
-	SOCKET newConnection;
+	ConnectSocket(cn);
 	
 	
 	system("pause");
