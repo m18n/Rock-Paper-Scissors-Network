91abdcc2813eb98e9722fa9cf371f777a76963b4
Misha <20xim04@gmail.com>
2020-10-26 13:15:59 +0200

    Add struct Player

--- Rock-Paper-Scissors-Network-Server/main.cpp
+++ Rock-Paper-Scissors-Network-Server/main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
+struct Player {
+	char login[50];
+	int password=0;
+	int score=0;
+};
 int main() {
 	cout << "start server\n";
-
+	
 	system("pause");
 	return 0;
 }
