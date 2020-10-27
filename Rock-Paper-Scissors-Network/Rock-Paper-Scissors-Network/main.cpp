0845a6cf8f5b1683cc883061bb4900d8bccbdeab
Ice159 <60782182+Ice159@users.noreply.github.com>
2020-10-27 14:30:42 +0200

    ServerSingIn

--- Rock-Paper-Scissors-Network/Rock-Paper-Scissors-Network/main.cpp
+++ Rock-Paper-Scissors-Network/Rock-Paper-Scissors-Network/main.cpp
@@ -2,7 +2,9 @@
 #pragma warning(disable:4996)
 #pragma comment(lib,"ws2_32.lib")
 #include<WinSock2.h>
+#include<string>
 using namespace std;
+SOCKET Conection;
 struct Player
 {
 	char login[20]="";
@@ -16,7 +18,7 @@ void Conect()
 	addr.sin_addr.s_addr = inet_addr("77.121.173.140");
 	addr.sin_port = htons(8080);
 	addr.sin_family = AF_INET;
-	SOCKET Conection = socket(AF_INET, SOCK_STREAM, NULL);
+	Conection = socket(AF_INET, SOCK_STREAM, NULL);
 	if (connect(Conection, (SOCKADDR*)&addr, sizeof(addr))!=0) 
 	{
 		system("cls");
@@ -41,14 +43,35 @@ struct Serwerconect
 };
 void SingIn(char login[20],int& password)
 {
+	char temp[10]; 
+	char score[10];
+	char passwordchar[20];
 	cout << "Login:";
 	cin >> login;
 	cout << "Password:";
 	cin >> password;
+	string paswordserwer;
+	paswordserwer = to_string(password);
+	strcpy_s(passwordchar, paswordserwer.c_str());
+	int size_passwoed = paswordserwer.length() + 1;
+	send(Conection, "i", 2, NULL);
+	send(Conection, login, strlen(login)+1, NULL);
+	send(Conection, passwordchar,size_passwoed , NULL);
+	recv(Conection, temp, sizeof(temp), NULL); 
+	recv(Conection, score, sizeof(score), NULL);
+	if (strcmp(temp,"ok")==0)
+	{
+		cout << "Sucsesful";
+	}
+	else 
+	{
+		cout << "Error:you computer alahatbar\n";
+	}
 }
 void SingUp(char login[20], int& password)
 {
 	int password1=0;
+	string paswordserwer;
 	system("cls");
 	cout << "Login:";
 	cin >> login;
@@ -62,6 +85,10 @@ void SingUp(char login[20], int& password)
 		cin >> password1;
 		system("cls");
 	} while (password != password1);
+	paswordserwer = to_string(password);
+	send(Conection, "u", 1, NULL);
+	send(Conection, login,strlen(login), NULL);
+	send(Conection, paswordserwer.c_str(), paswordserwer.length(), NULL);
 }
 void Login(Player& pl) {
 	int choice = 0;
